687e2f50931747a5807c81cc98e63996ac0e53aa
Takaharayuuki <takahara.yuuki22@gmail.com>
2022-04-12 18:38:01 +0900

    add: reset.css

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,26 @@ import router from './router'
 
 import liff from '@line/liff'
 
-async function main() {
+// CSS
+import './css/sanitize.css'
+
+// liff.init後にcreatAppを実行
+async function init() {
   const LIFF_ID = import.meta.env.VITE_APP_LIFF_ID as string
 
   await liff.init({ liffId: LIFF_ID })
+
   // if (!liff.isLoggedIn()) {
   //   liff.login()
   // }
 
-  createApp(App).use(router).mount('#app')
+  // Vueアプリを作成
+  const app = createApp(App)
+
+  // Vue Router
+  app.use(router)
+
+  app.mount('#app')
 }
 
-main()
+init()
