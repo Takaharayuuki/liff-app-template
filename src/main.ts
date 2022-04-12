1ac183c7f55a5e24fca663b74c987db61ea377c9
Takaharayuuki <takahara.yuuki22@gmail.com>
2022-04-12 18:16:33 +0900

    add: basefile

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import liff from '@line/liff'
+
+async function main() {
+  const LIFF_ID = import.meta.env.VITE_APP_LIFF_ID as string
+
+  await liff.init({ liffId: LIFF_ID })
+  // if (!liff.isLoggedIn()) {
+  //   liff.login()
+  // }
+
+  createApp(App).use(router).mount('#app')
+}
+
+main()
